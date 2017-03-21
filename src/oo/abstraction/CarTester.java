045fc53ef2cd5227e6d4436ccfb6e7d2f810ea33
user <user@es302nt>
2017-03-21 15:17:50 +0800

    Array in Java, primitive vs reference

--- src/oo/abstraction/CarTester.java
+++ src/oo/abstraction/CarTester.java
@@ -7,6 +7,15 @@ public class CarTester {
 		Car c2 = new Car("HONDA", "ACCORD", 2000);
 		Car c3 = new Car("TOYOTA", "WISH", 2000);
 
+		int[] n = new int[5];
+		System.out.println(n[2]);
+		
+		Car[] cars = new Car[3];
+		System.out.println(cars[0]);
+		cars[0] = new Car("TOYOTA", "ALTIS", 1800 );
+		cars[1] = new Car("HONDA", "ACCORD", 2000);
+		cars[2] = new Car("TOYOTA", "WISH", 2000);
+		System.out.println(cars[0].name);
 	}
 
 }
